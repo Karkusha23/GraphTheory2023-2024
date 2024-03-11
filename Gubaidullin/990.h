df0cd9447a91d950fba7f426c3bdc960fe70bb71
Karkusha23 <min.rinat567321@gmail.com>
2024-03-11 18:17:13 +0300

    Add task 990

---
+++ Gubaidullin/990.h
@@ -0,0 +1,70 @@
+#pragma once
+#include <vector>
+#include <string>
+
+class UnionFind
+{
+    std::vector<int> arr_;
+public:
+    UnionFind(int count) : arr_(count)
+    {
+        for (int i = 0; i < count; ++i)
+        {
+            arr_[i] = i;
+        }
+    }
+    int parent(int val)
+    {
+        return arr_[val] == val ? val : arr_[val] = parent(arr_[val]);
+    }
+    void unite(int first, int second)
+    {
+        arr_[parent(first)] = parent(second);
+    }
+    bool sameParent(int first, int second)
+    {
+        return parent(first) == parent(second);
+    }
+};
+
+class Solution 
+{
+public:
+    bool equationsPossible(std::vector<std::string>& equations) 
+    {
+        UnionFind unionFind(26);
+
+        for (std::string& str : equations)
+        {
+            int first = str[0] - 'a';
+            int second = str[3] - 'a';
+
+            if (str[0] == str[3])
+            {
+                if (str[1] == '=')
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (str[1] == '=')
+            {
+                unionFind.unite(str[0] - 'a', str[3] - 'a');
+            }
+        }
+
+        for (std::string& str : equations)
+        {
+            if (str[1] == '!' && unionFind.sameParent(str[0] - 'a', str[3] - 'a'))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
